9688edd6964ce5cae207440597b552f8eb8b952a
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-10-18 16:57:52 -0300

    PT1 - Corrigindo link de projetos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,37 +3,37 @@ const projects = [
   {
     name: 'Playground Functions',
     description: 'Segundo projeto do curso Trybe que buscou desenvolver minhas habilidades em Javascript com testes e raciocínio lógico focados em linguagem de programação.',
-    url: 'https://github.com/mthalmeida/portifolio/tree/main/meusProjetos/playgroundFunctions'
+    url: 'https://github.com/mthalmeida/portifolio/tree/master/meusProjetos/playgroundFunctions'
   },
 
   {
     name: 'Art Pixels',
     description: 'Terceiro projeto do curso Trybe com o objetivo de testar e aprimorar conhecimentos em Javascript, HTML e CSS com criação dinâmica e eventos de página.',
-    url: 'https://mthalmeida.github.io/portifolio/meusProjetos/pixelsArt/index.html'
+    url: 'https://htmlpreview.github.io/?https://github.com/mthalmeida/portifolio/blob/master/meusProjetos/pixelsArt/index.html'
   },
 
   {
     name: 'Trybewarts',
     description: 'Desenvolvimento de uma página de formulário da Escola de Magia de Trybewarts, em que as pessoas estudantes podem enviar seus feedbacks sobre ela. O tema desse projeto é baseado na obra Harry Potter, de J. K. Rowling.',
-    url: 'https://mthalmeida.github.io/portifolio/meusProjetos/trybeWarts/index.html'
+    url: 'https://htmlpreview.github.io/?https://github.com/mthalmeida/portifolio/blob/master/meusProjetos/trybeWarts/index.html'
   },
 
   {
     name: 'Lista de tarefas',
     description: 'Desenvolvido um site para organizar uma listagem de tarefas utilizando apenas linguagem JS e CSS com auxilo de Framework.',
-    url: 'https://mthalmeida.github.io/portifolio/meusProjetos/todoList/index.html'
+    url: 'https://htmlpreview.github.io/?https://github.com/mthalmeida/portifolio/blob/master/meusProjetos/todoList/index.html'
   },
 
   {
     name: 'Zoo Functions',
     description: 'Desenvolvimento de funções que buscam informações sobre as informações de um zoológico como: espécie, local de origem, dados sobre as pessoas que colaboram com a manutenção e cuidado do zoológico.',
-    url: ''
+    url: 'https://github.com/mthalmeida/portifolio/tree/master/meusProjetos/zooFunctions'
   },
 
   {
     name: 'JS Async',
     description: 'Uso de API que retorna um TOP 10 das principais moedas do mercado, utilizando CSS, HTML e API externa com uso de testes unitarios de verificação.',
-    url: ''
+    url: 'https://github.com/mthalmeida/portifolio/tree/master/meusProjetos/JS%20Async'
   },
 
   {
