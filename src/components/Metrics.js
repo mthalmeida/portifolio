2b4fc051b45850209b5dd43d92fe81c701c5b4c2
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-12-03 23:44:18 -0300

    fix: corrige responsividade 2

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -5,9 +5,8 @@ export default class Header extends React.Component {
   render() {
     return (
       <div id="Metricas" class="githubcardProject">
-          <a href="https://github.com/mthalmeida"/>
-          <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=mthalmeida&layout=compact&langs_count=7&theme=dracula"/>
-          <img height="180em" src="https://github-readme-stats.vercel.app/api?username=mthalmeida&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
+          <img height="180em" alt='Img1' src="https://github-readme-stats.vercel.app/api/top-langs/?username=mthalmeida&layout=compact&langs_count=7&theme=dracula"/>
+          <img height="180em" alt='Img2' src="https://github-readme-stats.vercel.app/api?username=mthalmeida&show_icons=true&theme=dracula&include_all_commits=true&count_private=true"/>
       </div>
       
     );
