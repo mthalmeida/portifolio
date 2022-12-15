58aa6152b755f5189d4f84e658284728e3387d0c
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-12-15 18:37:39 -0300

    feat: adicionando projetos ao data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -136,7 +136,7 @@ const projects = [
   {
     name: "App de Receitas",
     description: "<Em desenvolvimento>",
-    url: "",
+    url: " ",
     image:
       "https://images.unsplash.com/photo-1550063873-ab792950096b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
